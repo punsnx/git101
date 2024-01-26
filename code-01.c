ab854772d832bce53917c0eb2d29812963b1d61b
Sirisuk Tharntham <sirisuk.thmx47539@gmail.com>
2024-01-26 13:12:02 +0700

    new code02

--- code-01.c
+++ code-01.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World");
+    char text[] = "Hello Git";
+    printf("%s\n",text);
+
     return 0;
 }
---
+++ code-02.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // count : console output 
+    // endl : end line '\n'
+    cout << "Hello Git in C++" << endl;
+    return 0;
+}
